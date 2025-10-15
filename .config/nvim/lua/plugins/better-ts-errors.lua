526c63320c18e025270aefbe53d1b82e0d9b7a20
Patrick Bpds <patrickbpds@gmail.com>
2025-10-15 10:34:24 -0300

    feat: add better TypeScript errors plugin
    Enhances TypeScript error display in Neovim. This plugin provides improved error messages, better formatting, and more informative diagnostics for TypeScript development.

---
+++ .config/nvim/lua/plugins/better-ts-errors.lua
@@ -0,0 +1,10 @@
+return {
+  "OlegGulevskyy/better-ts-errors.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {
+    keymaps = {
+      toggle = "<leader>xd",
+      go_to_definition = "<leader>xD",
+    },
+  },
+}
