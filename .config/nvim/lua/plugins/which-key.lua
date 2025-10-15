f2f4838cb11ab1ea41d1b4480837895e26c13638
Patrick Bpds <patrickbpds@gmail.com>
2025-10-15 10:34:25 -0300

    feat: add Which-key plugin configuration
    Sets up the Which-key plugin for key binding hints. This plugin shows available key combinations as you type, helping users discover and remember keyboard shortcuts.

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      preset = "classic",
+     spec = {
+       { "<leader>a", group = "ai", icon = { icon = "", color = "yellow" } },
+       { "<leader>z", group = "scripts", icon = { icon = "", color = "cyan" } },
+       { "<leader>m", group = "grapple" },
+       { "<leader>J", group = "Java", icon = { icon = "☕", color = "orange" } },
+     },
+    },
+  },
+}
